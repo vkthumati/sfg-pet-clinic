8790db292eaf03493dc13af1af7e147da8d1b08f
vkthumati <info2vasanth@gmail.com>
2019-03-17 19:15:52 -0500

    Adding POJO's. Closes #2

---
+++ src/main/java/com/vkthumati/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.vkthumati.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/vkthumati/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.vkthumati.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
