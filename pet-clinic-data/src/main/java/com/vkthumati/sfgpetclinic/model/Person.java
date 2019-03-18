b3a06b55f0c6841454a4e9e800aec4272b06eb1f
vkthumati <info2vasanth@gmail.com>
2019-03-18 13:58:08 -0500

    Added Pet Clinic Index Page and Controller : Closes #8

--- pet-clinic-data/src/main/java/com/vkthumati/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/vkthumati/sfgpetclinic/model/Person.java
@@ -1,7 +1,5 @@
 package com.vkthumati.sfgpetclinic.model;
 
-import com.sun.xml.internal.rngom.parse.host.Base;
-
 public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
