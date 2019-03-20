76ca33ed7b3cf8f3e4610961cd2bfc8bc6a44c3c
vkthumati <info2vasanth@gmail.com>
2019-03-20 18:14:41 -0500

    Listing all Vets on Vet index page: Closes #15

--- pet-clinic-web/src/main/java/com/vkthumati/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/vkthumati/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package com.vkthumati.sfgpetclinic.controllers;
 
+import com.vkthumati.sfgpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService){
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
