edcecde244dd651b62f831ff8b4b9c92188a6796
vkthumati <info2vasanth@gmail.com>
2019-03-22 18:46:05 -0500

    Create Specialty Map Service Impl: Closes #31

---
+++ pet-clinic-data/src/main/java/com/vkthumati/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,7 @@
+package com.vkthumati.sfgpetclinic.services;
+
+import com.vkthumati.sfgpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long>{
+
+}
---
+++ pet-clinic-data/src/main/java/com/vkthumati/sfgpetclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,35 @@
+package com.vkthumati.sfgpetclinic.services.map;
+
+import com.vkthumati.sfgpetclinic.model.Specialty;
+import com.vkthumati.sfgpetclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty domain) {
+        return super.save(domain);
+    }
+
+    @Override
+    public void delete(Specialty domain) {
+        super.delete(domain);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
