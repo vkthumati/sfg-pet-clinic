2c81b73667997d7c0fabcbe582ad09116d5b460f
vkthumati <info2vasanth@gmail.com>
2019-03-22 16:58:54 -0500

    Created Pet Type Map Service Impl: Closes #29

---
+++ pet-clinic-data/src/main/java/com/vkthumati/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.vkthumati.sfgpetclinic.services;
+
+import com.vkthumati.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/vkthumati/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,37 @@
+package com.vkthumati.sfgpetclinic.services.map;
+
+import com.vkthumati.sfgpetclinic.model.Pet;
+import com.vkthumati.sfgpetclinic.model.PetType;
+import com.vkthumati.sfgpetclinic.services.PetService;
+import com.vkthumati.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType domain) {
+        return super.save(domain);
+    }
+
+    @Override
+    public void delete(PetType domain) {
+        super.delete(domain);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
