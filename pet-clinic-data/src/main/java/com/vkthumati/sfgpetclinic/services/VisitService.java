1a38928216661dd31cb02304099134d007a6071b
vkthumati <info2vasanth@gmail.com>
2019-03-23 22:01:43 -0500

    Created Visit Map Service.Closes #46

---
+++ pet-clinic-data/src/main/java/com/vkthumati/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.vkthumati.sfgpetclinic.services;
+
+import com.vkthumati.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/vkthumati/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,40 @@
+package com.vkthumati.sfgpetclinic.services.map;
+
+import com.vkthumati.sfgpetclinic.model.Visit;
+import com.vkthumati.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit domain) {
+
+        if(domain.getPet()==null || domain.getPet().getOwner()==null || domain.getPet().getId()==null || domain.getPet().getOwner().getId()==null){
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(domain);
+    }
+
+    @Override
+    public void delete(Visit domain) {
+        super.delete(domain);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
