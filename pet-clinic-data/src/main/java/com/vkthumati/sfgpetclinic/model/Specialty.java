9e9bbd41340312238e0294f5a228d27e2b47eecb
vkthumati <info2vasanth@gmail.com>
2019-03-23 19:39:55 -0500

    Converted Vets to JPA Entities. Closes #37

--- pet-clinic-data/src/main/java/com/vkthumati/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/vkthumati/sfgpetclinic/model/Specialty.java
@@ -1,6 +1,14 @@
 package com.vkthumati.sfgpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Specialty extends BaseEntity {
+
+    @JoinColumn(name = "description")
     private String description;
 
     public String getDescription() {
