90017dd06582117a7de3a4f33c6473cf1e7fe7dc
vkthumati <info2vasanth@gmail.com>
2019-03-23 19:28:35 -0500

    Convert owners to JPA Entities: Closes #36

--- pet-clinic-data/src/main/java/com/vkthumati/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/vkthumati/sfgpetclinic/model/PetType.java
@@ -1,6 +1,14 @@
 package com.vkthumati.sfgpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
+
+    @JoinColumn(name = "name")
     private String name;
 
     public String getName() {
