99686d352d9d3941678bb72fb6320f89f49c6b39
vkthumati <info2vasanth@gmail.com>
2019-04-05 12:09:26 -0500

    Add ability to add or update a Pet. Close #56

--- pet-clinic-data/src/main/java/com/vkthumati/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/vkthumati/sfgpetclinic/model/Pet.java
@@ -1,6 +1,7 @@
 package com.vkthumati.sfgpetclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -11,10 +12,22 @@ import java.util.Set;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity{
+
+    @Builder
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
+        super(id);
+        this.name = name;
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+
+        if (visits == null || visits.size() > 0 ) {
+            this.visits = visits;
+        }
+    }
 
     @Column(name = "name")
     private String name;
@@ -28,6 +41,7 @@ public class Pet extends BaseEntity{
     private Owner owner;
 
     @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
--- pet-clinic-data/src/test/java/com/vkthumati/sfgpetclinic/services/springdatajpa/OwnerJpaServiceImplTest.java
+++ pet-clinic-data/src/test/java/com/vkthumati/sfgpetclinic/services/springdatajpa/OwnerJpaServiceImplTest.java
@@ -16,14 +16,14 @@ import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class OwnerJpaServiceImplTest {
 
     public static final String LAST_NAME = "Smith";
-
     @Mock
     OwnerRepository ownerRepository;
 
@@ -34,25 +34,35 @@ class OwnerJpaServiceImplTest {
     PetTypeRepository petTypeRepository;
 
     @InjectMocks
-    OwnerJpaServiceImpl ownerJpaService;
+    OwnerJpaServiceImpl service;
 
-    Long ownerId = 1L;
-    Owner returnedOwner;
+    Owner returnOwner;
 
     @BeforeEach
     void setUp() {
-        returnedOwner = Owner.builder().id(ownerId).lastName(LAST_NAME).build();
+        returnOwner = Owner.builder().id(1l).lastName(LAST_NAME).build();
+    }
+
+    @Test
+    void findByLastName() {
+        when(ownerRepository.findByLastName(any())).thenReturn(returnOwner);
+
+        Owner smith = service.findByLastName(LAST_NAME);
+
+        assertEquals(LAST_NAME, smith.getLastName());
+
+        verify(ownerRepository).findByLastName(any());
     }
 
     @Test
     void findAll() {
         Set<Owner> returnOwnersSet = new HashSet<>();
-        returnOwnersSet.add(Owner.builder().id(1L).build());
-        returnOwnersSet.add(Owner.builder().id(2L).build());
+        returnOwnersSet.add(Owner.builder().id(1l).build());
+        returnOwnersSet.add(Owner.builder().id(2l).build());
 
         when(ownerRepository.findAll()).thenReturn(returnOwnersSet);
 
-        Set<Owner> owners = ownerJpaService.findAll();
+        Set<Owner> owners = service.findAll();
 
         assertNotNull(owners);
         assertEquals(2, owners.size());
@@ -60,9 +70,9 @@ class OwnerJpaServiceImplTest {
 
     @Test
     void findById() {
-        when(ownerRepository.findById(anyLong())).thenReturn(Optional.of(returnedOwner));
+        when(ownerRepository.findById(anyLong())).thenReturn(Optional.of(returnOwner));
 
-        Owner owner = ownerJpaService.findById(ownerId);
+        Owner owner = service.findById(1L);
 
         assertNotNull(owner);
     }
@@ -71,46 +81,37 @@ class OwnerJpaServiceImplTest {
     void findByIdNotFound() {
         when(ownerRepository.findById(anyLong())).thenReturn(Optional.empty());
 
-        Owner owner = ownerJpaService.findById(ownerId);
+        Owner owner = service.findById(1L);
 
         assertNull(owner);
     }
+
 
     @Test
     void save() {
         Owner ownerToSave = Owner.builder().id(1L).build();
 
-        when(ownerRepository.save(any())).thenReturn(returnedOwner);
+        when(ownerRepository.save(any())).thenReturn(returnOwner);
 
-        Owner savedOwner = ownerJpaService.save(ownerToSave);
+        Owner savedOwner = service.save(ownerToSave);
 
         assertNotNull(savedOwner);
+
+        verify(ownerRepository).save(any());
     }
 
     @Test
     void delete() {
-        ownerJpaService.delete(returnedOwner);
+        service.delete(returnOwner);
 
-        //default is 1 time
+        //default is 1 times
         verify(ownerRepository, times(1)).delete(any());
     }
 
     @Test
     void deleteById() {
-        ownerJpaService.deleteById(ownerId);
+        service.deleteById(1L);
 
-        //default is 1 time
-        verify(ownerRepository, times(1)).deleteById(anyLong());
-    }
-
-    @Test
-    void findByLastName() {
-        when(ownerRepository.findByLastName(any())).thenReturn(returnedOwner);
-
-        Owner owner = ownerJpaService.findByLastName(LAST_NAME);
-
-        assertEquals(LAST_NAME, owner.getLastName());
-
-        verify(ownerRepository).findByLastName(any());
+        verify(ownerRepository).deleteById(anyLong());
     }
 }
